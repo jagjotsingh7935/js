2f5d61a782443edd16f438fcb9229d4e59e9e44d
Jagjot Singh <109655748+jagjotsingh7935@users.noreply.github.com>
2024-06-12 18:27:26 +0000

    Datatypesconversion commit

---
+++ 01_basics/03_Datatypesconversion.js
@@ -0,0 +1,29 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let vauleInNumber = Number(score);
+
+// console.log(typeof vauleInNumber);
+// console.log(vauleInNumber);
+
+ // "33" => 33
+// "33abc" => NaN
+// true => 1; false=>0;
+
+let isLoggedIn = ""
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoogedIn);
+
+//1 => true; 0=>false
+// "" => false
+// "Jajgot"=> True
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);

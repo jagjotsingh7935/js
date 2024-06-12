39e327f249f52d31145019e319eac40a4cb70782
Jagjot Singh <109655748+jagjotsingh7935@users.noreply.github.com>
2024-06-12 20:04:36 +0000

    operations

--- 01_basics/03_Datatypesconversion.js
+++ 01_basics/03_Datatypesconversion.js
@@ -7,7 +7,7 @@
 // console.log(typeof vauleInNumber);
 // console.log(vauleInNumber);
 
- // "33" => 33
+// "33" => 33
 // "33abc" => NaN
 // true => 1; false=>0;
 
@@ -27,3 +27,34 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//************OPERATIONS**************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);  //2 raise to power
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Jajgot"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3+4) * 5 % 3);
+
+let game = 100
+game++;
+console.log(game);

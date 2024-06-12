ac4b58570cec7ccffcfc868eb6353faebb07893a
Jagjot Singh <109655748+jagjotsingh7935@users.noreply.github.com>
2024-06-12 17:53:26 +0000

    Commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "jagjotsingh2287@gmail.com"
+var accountPassword = "12345"
+accountcity = "Doraha"
+
+// accountId = 2   can't change because writing const for it
+
+accountEmail = "abc@gmail.com"
+accountPassword = "121212"
+accountcity = "Ludhiana"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountcity,accountPassword])

78d1d2b3e37086e8f094466c12fc49b79a746479
Jagjot Singh <109655748+jagjotsingh7935@users.noreply.github.com>
2024-06-12 18:11:17 +0000

    Datatypes commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs not browser
+
+console.log(3 + 3) 
+
+let name = "Jagjot"      //String data type
+let age = 21             //Number data type
+let isLoggedIn = false   //Boolean Data type
+
+//PRIMMITIVE DATATYPES
+ //number
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined =>
+ //symbol => unique
+
+//OBJECT DATAType
+
+console.log(typeof age);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
+
+console.log(age);

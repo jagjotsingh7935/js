ebfadb8046f1f911fd0b2eab0a249fd6b3ac4f8b
Jagjot Singh <109655748+jagjotsingh7935@users.noreply.github.com>
2024-06-13 21:06:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function addition() {
+    
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+
+
+function substraction() {
+    
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   
+    document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function multiplication() {
+    
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function division() {
+   
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+    
+    document.getElementById("result").innerHTML = num1 / num2;
+} 

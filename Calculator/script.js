20296be066f00c283480554a230d3b1cc43fdad6
Jagjot Singh <109655748+jagjotsingh7935@users.noreply.github.com>
2024-06-25 01:31:39 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,27 @@
+// script.js
+let displayValue = '';
+
+function appendNumber(number) {
+    displayValue += number;
+    document.getElementById('display').value = displayValue;
+}
+
+function appendOperator(operator) {
+    displayValue += operator;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        displayValue = '';
+    }
+}
